5d536163b52a9356de8fcdb47a0a75e98bb942af
Urko Azkarate <urko.azkarate@maristak.net>
2023-02-23 13:58:18 +0100

    Update 2.0

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
 let usuarios = [
-    {nombre: "jorge", contrasenya: 123, rol: "admin"},
-    {nombre: "javier", contrasenya: 123, rol: "profe"},
-    {nombre: "pedro", contrasenya: 123, rol: "alumno"},
+    {nombre: "leire", contrasena: 123, rol: "adminweb"},
+    {nombre: "urko", contrasena: 123, rol: "alumno"},
+    {nombre: "gorka", contrasena: 123, rol: "profesor"},
+    {nombre: "asua", contrasena: 123, rol: "familia"}
 ];
 
 
-function iniciarSesion (v_usuario, v_contrasenya, v_rol){
-    var v_usuario = document.getElementById("").value;
-    var v_contrasenya = document.getElementById("").value;
-    var v_rol = document.getElementById("").value;
+function login(v_usuario, v_contrasena, v_rol){
+    var v_usuario = document.getElementById("Usuario").value;
+    var v_contrasena = document.getElementById("Contraseña").value;
+    var v_rol = 0;
+
+    for (user of usuarios) {
+        if (user.nombre == v_usuario && user.contrasena == v_contrasena) {
+            var rol = user.rol;
+        }
+    }
 
 
-    if (rol == "admin") {
-        window.location.href="admin.html";
+    if (rol == "adminweb") {
+        window.location.href="adminweb.html";
      }
-     else if (rol == "profe") {
+     else if (rol == "profesor") {
          window.location.href="profe.html";
      }
      else if (rol == "alumno") {
          window.location.href="alumno.html";
      }
+     else if (rol == "familia"){
+        window.location.href="familia.html";
+     }
      else {
-         document.getElementById("texto").innerHTML = "Registrate e inténtalo otra vez.";
+         alert("Esos datos no son correctos. Regístrate o cambia los datos a unos válidos");
          }
+}
+
+function Enviar(){ /*función enviar para que salga una alerta en recuperarContra.html*/
+    alert("Recibirá un correo electrónico con instrucciones para restablecer su contraseña. Si no llega, asegúrese de revisar su carpeta de spam");
+} 
+
+function volverAtras() {
+    history.back();
 }
